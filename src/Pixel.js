55afdd56b61a617cd92fe7cd003a7f649c4130e2
Tom Raybould <Thomaseraybould@gmail.com>
2019-03-23 15:16:48 -0400

    Pixel object created

---
+++ src/Pixel.js
@@ -0,0 +1,16 @@
+
+class Pixel {
+
+    constructor () {
+        //The original position of the pixel before any scrambling
+        this.truePostion;
+        
+        this.currentPostion;
+
+        this.red;
+        this.green; 
+        this.blue;
+        this.alpha;
+    }
+    
+}
