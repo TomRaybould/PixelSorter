6c8e9603def293f1498d444e8b21cb6b00518136
Tom Raybould <Thomaseraybould@gmail.com>
2019-05-06 22:11:01 -0400

    Styling moved to logo component

--- src/Logo.js
+++ src/Logo.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-
 import PixelSorter from './pixel-sorter-component/PixelSorter'
+import "./Logo.css"
 
 class Logo extends Component {
     render() {
         return (
-            <div>
+            <div className="logo">
                 <PixelSorter
                     imageFileUrl = 'pixel-sorter-logo.png'
-                    algo = {"heapSort"}
+                    algo = {"quickSort"}
                     loop = {true}
                     scramble = {true}
-                    afterSortDelay = {1000}
-                />          
+                    afterSortDelay = {1000}/>          
             </div>
         );
     }
