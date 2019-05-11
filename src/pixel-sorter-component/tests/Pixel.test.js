7da1141af1fe8ff9b6c461e8a4aa4c83473a4405
Tom Raybould <Thomaseraybould@gmail.com>
2019-05-11 11:21:08 -0400

    First jest unit test added and working

---
+++ src/pixel-sorter-component/tests/Pixel.test.js
@@ -0,0 +1,22 @@
+import Pixel from '../Pixel.js'
+
+
+test('pixel comparedTo method', () => {
+    const pixel1 = new Pixel();
+    pixel1.truePosition = 0;
+    const pixel2 = new Pixel();
+    pixel2.truePosition = 0;
+
+    console.log(pixel1);
+
+    expect(pixel1.comparedTo(pixel2)).toBe(0);
+    
+    pixel1.truePosition = 1;
+    
+    expect(pixel1.comparedTo(pixel2)).toBe(1);
+
+    pixel1.truePosition = -1;
+
+    expect(pixel1.comparedTo(pixel2)).toBe(-1);
+
+});
